5c91b1750db38305de4fc15bf9cbd802a83b4f2d
23f3001827 <23f3001827@ds.study.iitm.ac.in>
2025-02-12 19:46:57 +0530

    Implemented user registration and admin login functionality

---
+++ controllers/auth.py
@@ -0,0 +1,22 @@
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+from models import User, db
+from app import app
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        flash("Invalid credentials!")
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
