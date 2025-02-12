2be3671a9c669beb14c36798148b7701a87306ef
23f3001827 <23f3001827@ds.study.iitm.ac.in>
2025-02-12 21:15:14 +0530

    Built Admin dashboard with CRUD operations for subjects, chapters, quizzes, and questions

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,26 +1,25 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db, Subject, Chapter, Quiz, Question
+from models import db, Quiz, Chapter
 from manage import app
+from datetime import datetime
 
 @app.route('/admin')
 def admin_dashboard():
-    subjects = Subject.query.all()
-    return render_template('admin_dashboard.html', subjects=subjects)
+    quizzes = Quiz.query.all()
+    return render_template('admin_dashboard.html', quizzes=quizzes)
 
-@app.route('/add_subject', methods=['POST'])
-def add_subject():
-    name = request.form['name']
-    new_subject = Subject(name=name)
-    db.session.add(new_subject)
-    db.session.commit()
-    flash('Subject added successfully!')
-    return redirect(url_for('admin_dashboard'))
+@app.route('/add_quiz', methods=['GET', 'POST'])
+def add_quiz():
+    if request.method == 'POST':
+        chapter_id = request.form['chapter_id']
+        date_of_quiz = datetime.strptime(request.form['date_of_quiz'], '%Y-%m-%d %H:%M')
+        duration = int(request.form['duration'])
 
-@app.route('/delete_subject/<int:subject_id>')
-def delete_subject(subject_id):
-    subject = Subject.query.get(subject_id)
-    if subject:
-        db.session.delete(subject)
+        new_quiz = Quiz(chapter_id=chapter_id, date_of_quiz=date_of_quiz, duration=duration)
+        db.session.add(new_quiz)
         db.session.commit()
-        flash('Subject deleted successfully!')
-    return redirect(url_for('admin_dashboard'))
+        flash('Quiz added successfully with time scheduling!')
+        return redirect(url_for('admin_dashboard'))
+    
+    chapters = Chapter.query.all()
+    return render_template('add_quiz.html', chapters=chapters)
