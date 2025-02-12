767d228422843061f7f8525174e87defa68d300d
23f3001827 <23f3001827@ds.study.iitm.ac.in>
2025-02-12 19:39:14 +0530

    Created database schema for users, subjects, quizzes, and scores

---
+++ models/__init__.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    full_name = db.Column(db.String(100))
+    qualification = db.Column(db.String(100))
+    dob = db.Column(db.Date)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+    name = db.Column(db.String(100), nullable=False)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'))
+    date_of_quiz = db.Column(db.Date)
+    duration = db.Column(db.Integer)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    question_statement = db.Column(db.Text)
+    option1 = db.Column(db.String(100))
+    option2 = db.Column(db.String(100))
+    option3 = db.Column(db.String(100))
+    option4 = db.Column(db.String(100))
+    correct_option = db.Column(db.Integer)
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    total_score = db.Column(db.Integer)
